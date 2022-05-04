94ea19bdb80aaa0583159c92cc8136cc74a30ba0
Rodrigo Lima <rcdl@cin.ufpe.br>
2022-05-04 03:42:40 -0300

    Update tiled imported assets

--- Assets/SuperTiled2Unity/Imported/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="map" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="map.png" width="288" height="32"/>
-</tileset>
