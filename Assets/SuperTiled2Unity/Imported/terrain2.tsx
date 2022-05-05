6e22e16e618cf6b3bc5ecc44f01f3eb96e72a6d3
Rodrigo Lima <rcdl@cin.ufpe.br>
2022-05-05 12:01:41 -0300

    Add v2 of map

---
+++ Assets/SuperTiled2Unity/Imported/terrain2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="terrain2" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="map2.png" width="256" height="128"/>
+</tileset>
