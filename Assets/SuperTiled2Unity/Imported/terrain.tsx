8a2abc070f575345ed4aecd6f3311f8815956d5e
Rodrigo Lima <rcdl@cin.ufpe.br>
2022-05-05 12:45:31 -0300

    Add supertiled2unity and import 2 map versions

---
+++ Assets/SuperTiled2Unity/Imported/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="terrain" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="map.png" width="288" height="32"/>
+</tileset>
